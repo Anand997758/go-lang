abeebb3a076458050f04c575d8f9c5c025fc6405
Anand997758 <reddyanand480@gmail.com>
2023-03-02 19:56:18 +0530

    02-03-2023

---
+++ go_program/for.go
@@ -0,0 +1,104 @@
+/*package main
+import
+func main() {
+	a := 1
+	b := 2
+	c := 3
+	d := 4.3
+	fmt.println(a, b, c, d)
+}*/
+
+/*package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}*/
+
+/*package main
+
+import "fmt"
+
+func main() {
+	var a = []int{1, 2, 3, 4, 5, 1, 3}
+	b := 2
+	_ = b
+	c := 5
+	_ = c
+	fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+
+}*/
+
+/*package main
+
+import "fmt"
+
+var a = 1
+var b = 2
+var c = 3
+var d = 2.3
+var e = "anand"
+
+func main() {
+	fmt.Println(a, b, c, d, e)
+}*/
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// var a = 2
+// 	var b = 3
+// 	var c float32 = float32(b)
+// 	fmt.Println(c)
+// }
+
+/*pacakge main
+import "fmt"
+func main() {
+	a int := 12
+	b float32 := 12.3
+	c float32 := float32(a)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}*/
+
+/*package main
+
+import "fmt"
+
+func main() {
+	var name = "anand"
+	var value = 1
+	var bool = true
+	var conver float32 = float32(value)
+	fmt.Println(name, value, bool, conver)
+}*/
+
+/*package main
+
+import "fmt"
+
+const PI = 3.1
+
+func main() {
+	fmt.Println(PI)
+}*/
+
+/*package main
+
+import "fmt"
+
+const A = 1
+
+func main() {
+	A := 2
+	fmt.Println(A)
+}*/
